6a09e489b0746ae80956e9831c0f8017590f4715
pirathipan <pirathipan.rupert-diknes@supinternet.fr>
2020-03-18 14:09:35 +0100

    add new project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,9 @@
 // sass
+import '../css/general/reset.scss'
 import '../css/app.scss';
 
 // css
-require("../css/general/reset.css");
 require('../css/app.css');
-require('../css/settings.css');
 require('../css/custom-style.css');
 require('../css/class/managers/clockManager.css');
 
@@ -18,6 +17,8 @@ import {Tabmenu} from "./class/Tabmenu/Tabmenu";
 import {Changeimage} from "./class/image/Changeimage";
 import {Checkbox} from "./class/checkbox/checkbox";
 import {Table} from "./class/Table/Table";
+import {Navbar} from "./class/Navbar/Navbar";
+
 require('../js/fontawesome/js/all')
 
 
@@ -34,10 +35,14 @@ change_image.changeimage();
 let table_products= new Table();
 table_products.table();
 
+
+
+let nav_bar= new Navbar();
+nav_bar.navbarleft();
+
 // let chech_box = new Checkbox();
 // chech_box.chech();
 
 
 const clock= new ClockManager();
 clock.enable();
-
