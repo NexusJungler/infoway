7d501cd3272f06e37a87d063703dde17decce20a
pira <prupert@infoway.fr>
2020-07-07 12:10:55 +0200

    motif popup media

--- assets/js/class/Tools/AnimationTool/ShowMediaFilter/ShowMediaFilter.js
+++ assets/js/class/Tools/AnimationTool/ShowMediaFilter/ShowMediaFilter.js
@@ -9,17 +9,17 @@ class ShowMediaFilter extends SubTool
         super();
         this.__name = this.constructor.name;
         this.__$container = $(".left_filter_container .filters_by_associated_data_container");
+        this.__$openFilter = $(".filters_by_associated_data_container");
     }
 
 
-    ClickShowFiltrer(active)
-    {
+    ClickShowFiltrer(active) {
 
         if(active)
         {
             this.__$container.find(".btn_show_filter").on("click.ClickShowFiltrer", e => {
 
-                let width = "35%"
+                let width = "35%";
 
                 if($(e.currentTarget).hasClass("btn_show_filter_open")){
                     this.__$container.find('.filter_container').addClass("filter_active");
@@ -48,11 +48,54 @@ class ShowMediaFilter extends SubTool
             this.__$container.find(".btn_show_filter").off("click.ClickShowFiltrer")
         }
 
+        return this;
+    }
+
+    ClickOpenFilterAssocier(active) {
+
+        if(active)
+        {
+            this.__$openFilter.find(".open_filter ").on("click.ClickOpenFilterAssocier", e => {
+
+                let width = "0%";
+
+                if($(e.currentTarget).hasClass("open_filter_show")){
+                    console.log('text');
+                    this.__$openFilter.find('.filter_container_association_open').addClass("filter_active");
+                    this.__$openFilter.find(".open_filter").removeClass("open_filter_show");
+                    $(e.currentTarget).find('i').removeClass('fa-plus').addClass('fa-minus');
+                    this.__$openFilter.find('.filter_container_association_open').css({"display":""})
+                    // this.__$openFilter.find('.filter').show();
+                    width = "100%" ;
+
+                } else {
+                    console.log("fini");
+                    this.__$openFilter.find('.filter_container_association_open').removeClass("filter_active");
+                    this.__$openFilter.find(".open_filter").addClass("open_filter_show");
+                    $(e.currentTarget).find('i').removeClass('fa-minus').addClass('fa-plus');
+                    // this.__$openFilter.find('.filter').hide();
+                    width = "0%" ;
+                }
 
 
+                $('.filter_container_association_open').animate({
+                    width: width
+                }, {
+                    queue: false,
+                    duration: 1000
+                });
+
+            })
+        }
+        else
+        {
+            this.__$openFilter.find(".open_filter ").off("click.ClickOpenFilterAssocier")
+        }
 
         return this;
     }
+
+
 
     ClickOpenMenubord(active){
         if(active){
@@ -75,12 +118,14 @@ class ShowMediaFilter extends SubTool
     {
         super.enable();
         this.ClickShowFiltrer(true)
+        this.ClickOpenFilterAssocier(true)
     }
 
     disable()
     {
         super.disable();
         this.ClickShowFiltrer(false)
+        this.ClickOpenFilterAssocier(false)
     }
 
 }
